7dafa42c4a3d2fbf30af5a9136142475e6aab767
hibhu <ramukvr@gmail.com>
2025-09-05 17:52:46 +0530

    initial add

---
+++ order-service/src/main/com/example/order/OrderApp.java
@@ -0,0 +1,11 @@
+package com.example.order;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        printOrderMessage();
+    }
+
+    public static void printOrderMessage() {
+        System.out.println("Order Service Processing Order...");
+    }
+}
---
+++ user-service/src/main/com/example/user/UserApp.java
@@ -0,0 +1,10 @@
+package com.example.user;
+
+import com.example.order.OrderApp;
+
+public class UserApp {
+    public static void main(String[] args) {
+        System.out.println("User Service Started!");
+        OrderApp.printOrderMessage();
+    }
+}
